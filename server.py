a6d2902ace73636f94d3be085b046a6581fe26f5
Arman-Salahshour <salahshour_a@yahoo.com>
2021-11-19 08:14:02 +0330

    create setTime function

--- server.py
+++ server.py
@@ -1,14 +1,52 @@
 import frameConvertor
 import imageToJsonConvertor
+import json
+from socket import socket
+from constants import *
 
-class Server:
-    def __init__(self):
-        pass
+# class Network:
+#     def __init__(self,host,port):
+#         self.host = host
+#         self.port = port
+    
+#     def server(self):
+#         with socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#             s.bind((self.host, self.port))
+#             s.listen()
+#             conn,addr=s.accept()
+#             with conn:
+#                 print(f"server is connected to {addr}")
+#                 while True:
+#                     data=conn.recv(size).encode(format)
+#                     if not data: 
+#                         break
+
+
+
+
+
+def setTime(data):
+    hours = 24 
+    length = int(hours/len(data))
+    time=0
+    for key in data:
+        data[key]['hour']=f"{time}_{time+length}"
+        time+=length+1
+
+    
+
+
+    
 
 
 def init():
-    frameConvertor.start(number=1000,gap=10)
+    frameConvertor.start(number=200,gap=5)
     json_img=imageToJsonConvertor.start()
+    json_imgDict=json.loads(json_img)
+    setTime(json_imgDict)
+    print(json_imgDict['SpiderMan']['hour'])
+
+
 
 
 
