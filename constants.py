e30aa3947b2c54b330982e9fe31d26f349128b0f
Arman-Salahshour <salahshour_a@yahoo.com>
2021-11-21 10:45:01 +0330

    add more message for showing in client side from server

--- constants.py
+++ constants.py
@@ -1,4 +1,13 @@
 host='127.0.0.1'
-port='65432'
-size=1024
+port=65432
+txt_messageSize=1024
+img_messageSize=4096
 format='utf-8'
+
+'''constant messages'''
+rqst_forImg='send current movie'
+end_moviesMsg="end of movies' schedule message"
+end_liveMovieNameMsg="end of live movie name message"
+end_liveMovieSizeMsg="end of live movie size message"
+end_liveMovieImgsMsg="end of live movie imgs message"
+end_sendingMsg='The end'
