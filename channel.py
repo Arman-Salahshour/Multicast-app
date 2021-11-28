2fa5d9f4f95ea18b22e24f87171806d5f448187d
Arman-Salahshour <salahshour_a@yahoo.com>
2021-11-28 16:26:56 +0330

    create method for receiving images

--- channel.py
+++ channel.py
@@ -7,7 +7,9 @@ class Network:
     def __init__(self,number):
         self.number= number
         self.data = []
-        self.start_channel()
+        ret=self.start_channel()
+        if ret:
+            self.receive_liveProgram()
 
     def start_channel(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
@@ -33,15 +35,33 @@ class Network:
             self.port=self.data['port']
             self.programs=self.data['programs']
             print(f'host:{self.host}\nport:{self.port}\nprograms:{self.programs}')
+            '''return value'''
+            ret=True
         except:
             '''if self.data does not include json file'''
-            pass
+            '''return value'''
+            ret=False
+        self.data=[]
+
+        return ret
     
     def receive_liveProgram(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
             soc.connect((host, port))
-        pass
-
+            msg=rqst_forImg+f':{self.number}'
+            soc.sendall(msg.encode(format))
+            while True:
+                recv=soc.recv(img_messageSize).decode(format)
+                if  recv==msg_receiving : 
+                    recv=msg_receivingImgs
+                    break
+                else:
+                    self.data.append(recv)
+            print(recv)
+        self.programs="".join(self.data)
+        self.programs=json.loads(self.programs)
+        print(self.programs['movies'])
 
 if __name__=='__main__':
-    network=Network(3)
+    network=Network(1)
+    
