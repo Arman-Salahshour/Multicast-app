b47c6fff9d4ec486afb4ef300d4814c3834c66d8
Arman-Salahshour <salahshour_a@yahoo.com>
2021-11-28 16:34:39 +0330

    add server_constants file

---
+++ server_constants.py
@@ -0,0 +1,15 @@
+host='127.0.0.1'
+port=65432
+txt_messageSize=1024
+img_messageSize=4096
+format='utf-8'
+
+'''constant messages'''
+rqst_forTimeSchedule="send programs' time schedule"
+rqst_forImg="send current movie"
+msg_active="msg: this chanel is active"
+end_moviesMsg="end of movies' schedule message"
+end_liveMovieNameMsg="end of live movie name message"
+end_liveMovieSizeMsg="end of live movie size message"
+end_liveMovieImgsMsg="end of live movie imgs message"
+end_sending="The end"
