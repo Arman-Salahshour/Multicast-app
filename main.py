665536e0fd04e0eaa813d48b5dd143035fba61a6
Arman-Salahshour <salahshour_a@yahoo.com>
2021-12-04 20:53:07 +0330

    write comments

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import threading
 import multiprocessing
-import json
 import server
 import client
-import time
 
 
+"""A class as a model for data which recieve from post request"""
 class User(BaseModel):
     id: str
     channel: int
-
+'''Start using fast api'''
 app=FastAPI()
 @app.post('/start/')
 async def get_img(user:User):
     try:
+        """make a process for each client"""
         process=multiprocessing.Process(target=client.init, args=(user.id,user.channel,))
         process.start()
         process.join()
@@ -28,5 +27,6 @@ async def get_img(user:User):
 
 
 if __name__ == '__main__':
+    '''make a process for running server and channels'''
     mainServer=multiprocessing.Process(target=server.init, args=('main',))
     mainServer.start()
