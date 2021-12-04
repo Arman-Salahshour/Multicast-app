601bb8000f250f9ab0df19a42befd2a76d509459
Arman-Salahshour <salahshour_a@yahoo.com>
2021-12-04 08:07:28 +0330

    create receive method function

--- client.py
+++ client.py
@@ -1,7 +1,10 @@
 import json
 import socket
+import base64
+from numpy import infty
 from client_constants import *
 import time
+import struct
 
 class Network:
     def __init__(self,id):
@@ -25,19 +28,59 @@ class Network:
         print(recv)
         self.channels_ipPort="".join(self.data)
         self.channels_ipPort=json.loads(self.channels_ipPort)
-        self.channels_ipPort={item['number']:{'host':item['host'], 'port':item['port']} for item in self.channels_ipPort}
-        print(f"channels: {self.channels_ipPort}")
+        self.channels_ipPort={item['number']:{'host':item['host'], 'port':item['port'], 'timing':json.loads(item['timing'])} for item in self.channels_ipPort}
+        print(f"channels are ready")
         self.data=[]
 
+    def receive_data(self,channel):
+        multicast_group=self.channels_ipPort[channel]['host']
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM,socket.IPPROTO_UDP) as soc:
 
+            soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            soc.bind(("",int(self.channels_ipPort[channel]['port'])))
+            group = socket.inet_aton(multicast_group)
+            mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+            soc.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
+            images=[]
+            packet,addr=soc.recvfrom(img_messageSize)
+            packet=packet.decode(format)
+            packet=json.loads(packet)
+            start=int(packet['num'])
+            index=int(packet['num'])
+            state=float('inf')
+            
+            while state != start:
+                image={
+                    'num':index,
+                    'img':[],
+                }
+                state=index
+                while state==index:
+                    image['img'].append(packet['img'])
+                    packet,addr=soc.recvfrom(img_messageSize)
+                    packet=packet.decode(format)
+                    packet=json.loads(packet)
+                    index=int(packet['num'])
+                state=index
 
+                image['img']="".join(image['img'])
+                images.append(image)
+                # print(start,index,state)
+            images=images[1:]
+            for img in images:
 
+                num=img['num']
+                temp=img['img']
+                temp=temp.encode(format)
+                temp=base64.decodebytes(temp)
+                with open(f'cache/index{num}.png','wb') as im:
+                    im.write(temp)
 
+            
 
-
-
-
+    
 
 if __name__ == "__main__":
     network=Network(1)
+    network.receive_data('1')
--- client_constants.py
+++ client_constants.py
@@ -2,11 +2,12 @@
 host='127.0.0.1'
 port=65432
 txt_messageSize=1024
-img_messageSize=4096
+img_messageSize=32000
 format='utf-8'
 
 '''constant messages'''
 rqst_forIpPort="client:send channels' ip and port"
+rqst_forImgs="client:send movie's frames"
 msg_receiving="The end"
 msg_receivingIpPort="msg: receive channels' ip and port"
 
